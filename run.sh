70bcc2808d547e79b0de1412315f4147763ffe19
huoyinghui <2285020853@qq.com>
2019-09-03 15:32:50 +0800

    update: run

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-docker run -d --name frp-server -v `pwd`/conf:/conf --restart=always cloverzrg/frps-docker
+docker run -d --name frp-server -v `pwd`/conf:/conf  -p 7000:7000 --restart=always cloverzrg/frps-docker
